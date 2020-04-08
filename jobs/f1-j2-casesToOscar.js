25cc2c07f6dd73b7532dc35889c21e185900ccb5
Taylor Downs <downs.taylor@gmail.com>
2020-04-08 12:11:31 -0600

    add mapping to oscar case load #2

--- jobs/f1-j2-casesToOscar.js
+++ jobs/f1-j2-casesToOscar.js
@@ -4,7 +4,7 @@ alterState((state) => {
   while (state.data.length) {
     results.push(state.data.splice(0, 100));
   }
-  
+
   state.caseChunks = results;
   return state;
 });
@@ -12,6 +12,26 @@ alterState((state) => {
 each(
   '$.caseChunks[*]',
   post('/api/v1/organizations/clients/create_many/', {
-    body: (state) => state.data,
+    body: (state) => {
+      return state.data.map((x) => {
+        return {
+          external_id: x.case_id,
+          external_id_display: x.case_id_display,
+          global_id: x.oscar_number,
+          mosvy_number: x.mosvy_number,
+          given_name: x.name_first,
+          family_name: x.name_last,
+          gender: x.sex,
+          date_of_birth: x.date_of_birth,
+          location_current_village_code: x.location_current,
+          reason_for_referral: x.protection_status,
+          external_case_worker_name: x.owned_by,
+          external_case_worker_id: x.owned_by_id,
+          organization_name: x.owned_by_agency,
+          organization_id: x.owned_by_agency_id,
+          'services[][name]': x['services_section[][service_type]'],
+        };
+      });
+    },
   })
 );
