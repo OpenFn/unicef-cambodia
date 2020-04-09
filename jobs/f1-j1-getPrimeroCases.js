7f9e21ac369d8d7538eed1b7e423d4931531c7e1
Taylor Downs <downs.taylor@gmail.com>
2020-04-09 09:18:01 -0600

    use registration date

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -1,7 +1,8 @@
 getCases({
   remote: true,
   scope: {
-    transitions_created_at: `dateRange||${state.lastFetch}.20-Apr-2021`,
+    registration_date: `date_range||${state.lastFetch}.22-Mar-2020`,
+    // transitions_created_at: `date_range||${state.lastFetch}.22-Mar-2020`,
     service_response_types: 'list||referral_to_oscar',
   },
 });
