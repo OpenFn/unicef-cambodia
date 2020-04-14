87e72e757b36db59acd04032ef091fa75042d450
Taylor Downs <downs.taylor@gmail.com>
2020-04-14 12:54:38 -0600

    confirmed get new cases working with proper username

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -1,8 +1,7 @@
 getCases({
   remote: true,
   scope: {
-    registration_date: `date_range||${state.lastFetch}.22-Mar-2020`,
-    // transitions_created_at: `date_range||${state.lastFetch}.22-Mar-2020`,
+    transitions_created_at: `date_range||${state.lastFetch}.30-Mar-2020`,
     service_response_types: 'list||referral_to_oscar',
   },
 });
