55d3c53f8183262709b8b2a410d47c4b1887a5b2
Taylor Downs <downs.taylor@gmail.com>
2020-04-20 12:19:17 -0600

    add mapping template for converting agency names/ids

--- jobs/f2-j2-upsertCasesToPrimero.js
+++ jobs/f2-j2-upsertCasesToPrimero.js
@@ -2,6 +2,56 @@
 // User Story 2: View Oscar cases in Primero
 // User Story 4: Sending referrals to Primero
 alterState(state => {
+  state.agencies = {
+    'agency-agh': 'agency-agh-user',
+    'agency-ahc': 'agency-ahc-user',
+    'agency-ajl': 'agency-ajl-user',
+    'agency-auscam': 'agency-auscam-user',
+    'agency-brc': 'agency-brc-user',
+    'agency-cccu': 'agency-cccu-user',
+    'agency-cct': 'agency-cct-user',
+    'agency-cfi': 'agency-cfi-user',
+    'agency-cif': 'agency-cif-user',
+    'agency-css': 'agency-css-user',
+    'agency-cvcd': 'agency-cvcd-user',
+    'agency-cwd': 'agency-cwd-user',
+    'agency-demo': 'agency-demo-user',
+    'agency-fco': 'agency-fco-user',
+    'agency-fit': 'agency-fit-user',
+    'agency-fsc': 'agency-fsc-user',
+    'agency-fsi': 'agency-fsi-user',
+    'agency-fts': 'agency-fts-user',
+    'agency-gca': 'agency-gca-user',
+    'agency-gct': 'agency-gct-user',
+    'agency-hfj': 'agency-hfj-user',
+    'agency-hol': 'agency-hol-user',
+    'agency-holt': 'agency-holt-user',
+    'agency-icf': 'agency-icf-user',
+    'agency-isf': 'agency-isf-user',
+    'agency-kmo': 'agency-kmo-user',
+    'agency-kmr': 'agency-kmr-user',
+    'agency-lwb': 'agency-lwb-user',
+    'agency-mande': 'agency-mande-user',
+    'agency-mho': 'agency-mho-user',
+    'agency-mrs': 'agency-mrs-user',
+    'agency-msl': 'agency-msl-user',
+    'agency-mtp': 'agency-mtp-user',
+    'agency-my': 'agency-my-user',
+    'agency-myan': 'agency-myan-user',
+    'agency-newsmile': 'agency-newsmile-user',
+    'agency-pepy': 'agency-pepy-user',
+    'agency-rok': 'agency-rok-user',
+    'agency-scc': 'agency-scc-user',
+    'agency-shk': 'agency-shk-user',
+    'agency-spo': 'agency-spo-user',
+    'agency-ssc': 'agency-ssc-user',
+    'agency-tlc': 'agency-tlc-user',
+    'agency-tmw': 'agency-tmw-user',
+    'agency-tutorials': 'agency-tutorials-user',
+    'agency-voice': 'agency-voice-user',
+    'agency-wmo': 'agency-wmo-user',
+  };
+
   state.cases = state.data.map(x => {
     //Mappings for upserting cases in Primero (update if existing, insert if new)
     return {
@@ -20,7 +70,7 @@ alterState(state => {
         oscar_status: x.status,
         case_status_reopened: true, // >>Q: This is inferred how?
         protection_status: x.reason_for_referral,
-        owned_by: 'government_user_location_one', // >>Q: What determines the owner/user?
+        owned_by: state.agencies[`agency-${x.organization_name}`],
         oscar_reason_for_exiting: x.reason_for_exiting,
         has_referral: 'true',
         consent_for_services: 'true',
@@ -40,9 +90,11 @@ alterState(state => {
           oscar_case_worker_telephone: x.case_worker_mobile,
         },
       ],
-      transitions: [{
-        // >>Q: Confirming we're not creating transitions?
-      }],
+      transitions: [
+        {
+          // >>Q: Confirming we're not creating transitions?
+        },
+      ],
     };
   });
 
@@ -52,7 +104,7 @@ alterState(state => {
 each(
   '$.cases[*]',
   upsertCase(
-    { externalId: 'oscar_number', data: state => state.data }, //>>Q: Upsert Primero cases based on matching oscar_number, mosvy_number, AND/OR case_id? 
+    { externalId: 'oscar_number', data: state => state.data }, //>>Q: Upsert Primero cases based on matching oscar_number, mosvy_number, AND/OR case_id?
     alterState(state => {
       return post(
         //Return case links to Oscar
