436412e52368feacfe12fc3ca28acb0d30d5cccd
Aleksa Krolls <aleksagk@gmail.com>
2020-04-23 04:12:46 -0700

    aleksa@openfn.org's change on OpenFn.org

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -3,7 +3,7 @@
 getCases({
   remote: true,
   scope: {
-    transitions_created_at: `date_range||${state.lastFetch}.30-Mar-2020`, //>>Q: What should this date filter be?
+    transitions_created_at: `date_range||${state.lastFetch}.30-Mar-2020`, //Request new updates since last fetch
     service_response_types: 'list||referral_to_oscar',
   },
 });
