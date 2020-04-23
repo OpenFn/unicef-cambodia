16708afa31401706451e2b63fcaa8f44b299263d
Taylor Downs <downs.taylor@gmail.com>
2020-04-23 12:28:11 -0600

    fixes #7, calls items in case array 'c' rather than 'x'

--- jobs/f2-j2-upsertCasesToPrimero.js
+++ jobs/f2-j2-upsertCasesToPrimero.js
@@ -1,42 +1,43 @@
 // Oscar cases ---> Primero
 // User Story 2: 'View Oscar cases in Primero' AND User Story 4: 'Sending referrals to Primero'
 alterState(state => {
-  state.cases = state.data.data.map(x => {
+  state.cases = state.data.data.map(c => {
     //Mappings for upserting cases in Primero (update if existing, insert if new)
     return {
       remote: true,
-      oscar_number: x.global_id,
+      oscar_number: c.global_id,
       child: {
         // primero_field: oscar_field,
-        oscar_number: x.global_id,
-        mosvy_number: x.mosvy_number,
-        name_first: x.given_name,
-        name_last: x.family_name,
-        sex: x.gender,
-        date_of_birth: x.date_of_birth,
-        location_current: x.location_current_village_code,
-        address_current: x.address_current_village_code,
-        oscar_status: x.status,
-        protection_status: x.reason_for_referral,
-        owned_by: `agency-${x.organization_name}-user`, //Q: Confirm naming convention 'agency-org-user'
-        oscar_reason_for_exiting: x.reason_for_exiting,
+        oscar_number: c.global_id,
+        mosvy_number: c.mosvy_number,
+        name_first: c.given_name,
+        name_last: c.family_name,
+        sex: c.gender,
+        date_of_birth: c.date_of_birth,
+        location_current: c.location_current_village_code,
+        address_current: c.address_current_village_code,
+        oscar_status: c.status,
+        protection_status: c.reason_for_referral,
+        owned_by: `agency-${c.organization_name}-user`, //Q: Confirm naming convention 'agency-org-user'
+        oscar_reason_for_exiting: c.reason_for_exiting,
         has_referral: 'true',
         consent_for_services: 'true',
         disclosure_other_orgs: 'true',
         module_id: "primeromodule-cp",
-        registration_date: x.referral_date
+        registration_date: c.referral_date
       },
-      services_section: [
-        {
-          //unique_id: x.??,  //Q: Is this a UUID from OSCaR or one that OpenFn generates? 
-          //service_type: x.services.name,  
-          //service_type_text: x.services.name, //Q: Same mapping as above? 
-          //service_type_details_text: x.services.name, //Q: Same mapping as above? 
-          oscar_case_worker_name: x.case_worker_name,
-          oscar_referring_organization: x.organization_name,
-          oscar_case_worker_telephone: x.case_worker_mobile,
+      services_section: c.services.map(s => {
+        return {
+          unique_id: s.id,  //Q: Is this a UUID from OSCaR or one that OpenFn generates? 
+          service_type: s.name,  
+          service_type_text: s.name, //Q: Same mapping as above? 
+          service_type_details_text: s.name, //Q: Same mapping as above? 
+          // These are not in the array, but come down from the case
+          oscar_case_worker_name: c.case_worker_name,
+          oscar_referring_organization: c.organization_name,
+          oscar_case_worker_telephone: c.case_worker_mobile,
         },
-      ],
+      ),
       transitions: [
         {
           //service_section_unique_id: x.??, //Q: How should we generate this UUID?
