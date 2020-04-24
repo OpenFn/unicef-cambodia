38c481aa36bacb62d428619a3d6c03405b362ccd
Taylor Downs <downs.taylor@gmail.com>
2020-04-24 14:56:03 -0600

    closes #11

--- jobs/f2-j2-upsertCasesToPrimero.js
+++ jobs/f2-j2-upsertCasesToPrimero.js
@@ -1,6 +1,21 @@
 // Oscar cases ---> Primero
 // User Story 2: 'View Oscar cases in Primero' AND User Story 4: 'Sending referrals to Primero'
 alterState(state => {
+  const serviceMap = {
+    'Social Work / Case Work': 'Social Work / Case Work',
+    'Family Based Care': 'Family Based Care',
+    'Drug/Alcohol': 'Drug / Alcohol',
+    Counselling: 'Counselling',
+    'Financial Development': 'Financial Development',
+    'Disability Support': 'Disability Support',
+    'Medical Support': 'Medical Support',
+    'Legal Support': 'Legal Support',
+    'Mental Health Support': 'Mental Health Support',
+    'Training and Education': 'Training and Education',
+    'Family Support': 'Family Support',
+    'Anti-Trafficking': 'Anti-Trafficking',
+  };
+
   state.cases = state.data.data.map(c => {
     //Mappings for upserting cases in Primero (update if existing, insert if new)
     return {
@@ -23,28 +38,28 @@ alterState(state => {
         has_referral: 'true',
         consent_for_services: 'true',
         disclosure_other_orgs: 'true',
-        module_id: "primeromodule-cp",
+        module_id: 'primeromodule-cp',
         registration_date: c.referral_date,
         oscar_case_worker_name: c.case_worker_name,
         oscar_referring_organization: c.organization_name,
-        oscar_case_worker_telephone: c.case_worker_mobile
+        oscar_case_worker_telephone: c.case_worker_mobile,
       },
       services_section: c.services.map(s => {
         return {
           unique_id: s.uuid,
-          service_type: s.name,  //Add service mapping table. If s.name not in table, list as "Other"
-          service_type_text: s.name, //Same logic as above
-          service_type_details_text: s.name //If type is "Other", map to services[][name][][subservices][name]
-        },
-      ),
-      transitions: [
-        {
-          service_section_unique_id: s.uuid,
-          service: s.services.name, //This is see above logic for service name mapping, array
-          created_at: s.referral_date, //Q: Should this be today's date?
-          type: 'referral'
-        },
-      ],
+          service_type: serviceMap[s.name] || 'Other', //Add service mapping table. If s.name not in table, list as "Other"
+          service_type_text: serviceMap[s.name] || 'Other', //Same logic as above
+          service_type_details_text: serviceMap[s.name] ? 'n/a' : s.name, //If type is "Other", map to services[][name][][subservices][name]
+        };
+      }),
+      transitions: c.services.map(t => {
+        return {
+          service_section_unique_id: t.uuid,
+          service: serviceMap[t.name] || 'Other', //This is see above logic for service name mapping, array
+          // created_at: t.referral_date, //Q: Should this be today's date?
+          type: 'referral',
+        };
+      }),
     };
   });
 
@@ -53,7 +68,8 @@ alterState(state => {
 
 each(
   '$.cases[*]',
-  upsertCase({ //Upsert Primero cases based on matching 'oscar_number' OR 'case_id'
+  upsertCase({
+    //Upsert Primero cases based on matching 'oscar_number' OR 'case_id'
     externalIds: ['oscar_number', 'case_id'],
     data: state => state.data,
   })
