c47ed0a8a166293370415a1d65ed1f604487dbe7
Taylor Downs <downs.taylor@gmail.com>
2020-04-24 11:57:56 -0600

    flip date string, per Alberto

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -4,21 +4,19 @@ getCases(
   {
     remote: true,
     scope: {
-      transitions_created_at: `date_range||${state.lastFetch}.30-Mar-3020`, //>>Q: What should this date filter be?
+      transitions_created_at: `date_range||${state.lastFetch}.30-03-3020`, //>>Q: What should this date filter be?
       service_response_types: 'list||referral_to_oscar',
     },
   },
   state => {
     // TODO: Is transitions enough, or do we need to use an updated_at field on the case? 
     // Get latest transition from all cases.
-    state.lastFetch = state.data.map(x => {
+    const dateArr = state.data.map(x => {
       // Get latest transition from a single case
       return x.transitions.map(t => t.created_at).sort((a, b) => b - a)[0];
-    }).sort((a, b) => b - a)[0];
+    }).sort((a, b) => b - a)[0].split('/');
 
-    console.log(state.lastFetch);
-    // TODO: The API returns dates in a very different format that it accepts dates.
-    // We will need to do some date conversion in here.
+    state.lastFetch = `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`
     return state;
   }
 );
