a54410f7096fa99b4cd995c3e5c4aa2131dda686
Taylor Downs <downs.taylor@gmail.com>
2020-04-29 07:58:42 -0600

    post an object for each item in the case array, #16

--- jobs/f1-j2-casesToOscar.js
+++ jobs/f1-j2-casesToOscar.js
@@ -1,19 +1,7 @@
 // Primero cases --> OSCaR
 // User Story 1: Generating government referrals, creating referrals in Oscar
-
-alterState(state => {
-  var results = [];
-
-  while (state.data.length) {
-    results.push(state.data.splice(0, 1)); //sending cases in batches of 1
-  }
-
-  state.caseChunks = results;
-  return state;
-});
-
 each(
-  '$.caseChunks[*]',
+  '$.data[*]',
   post(
     //Oscar authentication
     '/api/v1/auth/sign_in',
@@ -37,44 +25,43 @@ each(
         };
       },
       body: state => {
-        return state.references[1].map(c => {
-          //Mappings for posting cases to Oscar
-          const json = {
-            //oscar_field, primero_field,
-            external_id: c.case_id,
-            external_id_display: c.case_id_display,
-            global_id: c.oscar_number,
-            mosvy_number: c.mosvy_number,
-            given_name: c.name_first,
-            family_name: c.name_last,
-            gender: c.sex,
-            date_of_birth: c.date_of_birth.replace(/\//g, '-'),
-            location_current_village_code: c.location_current,
-            address_current_village_code: c.address_current,
-            reason_for_referral: c.protection_status,
-            external_case_worker_name: c.owned_by,
-            external_case_worker_id: c.owned_by_id,
-            external_case_worker_mobile_phone: c.owned_by_phone,
-            organization_name: 'cif', //hardcoding to one of the orgs in Oscar staging system for testing
-            //organization_name: c.owned_by_agency.substring(7),
-            organization_id: c.owned_by_agency_id,
-            services: c.services_section.map(s => {
-              return {
-                uuid: s.unique_id,
-                name: s.service_type,
-              };
-            }),
-            transaction_id: c.transition_id,
-          };
+        const c = state.references[1];
+        //Mappings for posting cases to Oscar
+        const json = {
+          //oscar_field, primero_field,
+          external_id: c.case_id,
+          external_id_display: c.case_id_display,
+          global_id: c.oscar_number,
+          mosvy_number: c.mosvy_number,
+          given_name: c.name_first,
+          family_name: c.name_last,
+          gender: c.sex,
+          date_of_birth: c.date_of_birth.replace(/\//g, '-'),
+          location_current_village_code: c.location_current,
+          address_current_village_code: c.address_current,
+          reason_for_referral: c.protection_status,
+          external_case_worker_name: c.owned_by,
+          external_case_worker_id: c.owned_by_id,
+          external_case_worker_mobile_phone: c.owned_by_phone,
+          organization_name: 'cif', //hardcoding to one of the orgs in Oscar staging system for testing
+          //organization_name: c.owned_by_agency.substring(7),
+          organization_id: c.owned_by_agency_id,
+          services: c.services_section.map(s => {
+            return {
+              uuid: s.unique_id,
+              name: s.service_type,
+            };
+          }),
+          transaction_id: c.transition_id,
+        };
 
-          // NOTE: Comment this out (or disable console) in production to protect
-          // against exposure of sensitive data.
-          console.log(
-            'Case data to be posted to Oscar: ',
-            JSON.stringify(json, null, 2)
-          );
-          return json;
-        });
+        // NOTE: Comment this out (or disable console) in production to protect
+        // against exposure of sensitive data.
+        console.log(
+          'Case data to be posted to Oscar: ',
+          JSON.stringify(json, null, 2)
+        );
+        return json;
       },
     })
   )
