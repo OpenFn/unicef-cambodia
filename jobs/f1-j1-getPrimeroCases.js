9ef41493edd6a802522f6605340f696220c7dd6f
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 15:06:44 -0600

    use a combination of date filters, per Traci and Alberto

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -4,15 +4,14 @@ getCases(
   {
     remote: true,
     scope: {
-      transitions_changed_at: `date_range||${
-         '01-01-2020' //default starting date
-      }.01-01-2020`, //effectively no end date (year 4000!)
-
-      // Replace date filter with transitions_changed_at once Quoin confirms questions
-      // transitions_changed_at: `date_range||${
-      //   state.lastFetch || '01-01-2020'
-      // }.01-01-4000`,
-
+      or: {
+        transitions_created_at: `or_op||date_range||${
+          state.lastCreated || '01-01-2020'
+        }.01-01-4020`,
+        transitions_changed_at: `or_op||date_range||${
+          state.lastUpdated || '01-01-2020'
+        }.01-01-4020`,
+      },
       service_response_types: 'list||referral_to_oscar',
     },
   },
@@ -20,21 +19,37 @@ getCases(
     state.references = [];
     // TODO: Is transitions enough, or do we need to use an updated_at field on the case?
     // Get latest transition from all cases.
-    const dateArr = state.data
+    const creationDates = state.data
       .map(x => {
         // Get latest transition from a single case
         return x.transitions.map(t => t.created_at).sort((a, b) => b - a)[0];
       })
       .sort((a, b) => b - a);
 
-    const dateParts = dateArr.length > 0 && dateArr[0].split('/');
+    const lastCreationParts = creationDates.length > 0 && dateArr[0].split('/');
 
-    if (dateParts) {
-      console.log("Found cases, updating 'last fetched' date.");
-      state.lastFetch = `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`;
+    if (lastCreationParts) {
+      console.log("Found cases, updating 'last created case' date.");
+      state.lastCreated = `${lastCreationParts[2]}-${lastCreationParts[1]}-${lastCreationParts[0]}`;
     }
 
-    console.log('The last fetched transition date is: ' + state.lastFetch);
+    const updateDates = state.data
+      .map(x => {
+        // Get latest transition from a single case
+        return x.transitions.map(t => t.changed_at).sort((a, b) => b - a)[0];
+      })
+      .sort((a, b) => b - a);
+
+    const lastUpdateParts = updateDates.length > 0 && dateArr[0].split('/');
+
+    if (lastUpdateParts) {
+      console.log("Found cases, updating 'last updated case' date.");
+      state.lastUpdated = `${lastCreationParts[2]}-${lastCreationParts[1]}-${lastCreationParts[0]}`;
+    }
+
+    console.log('The last transition creation is: ' + state.lastCreated);
+    console.log('The last transition update is: ' + state.lastUpdated);
+
     return state;
   }
 );
