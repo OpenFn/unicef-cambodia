79e3a9ca0634ed912571d4b71a98854dad2730e2
Aleksa Krolls <aleksagk@gmail.com>
2020-05-04 17:57:48 -0700

    aleksa@openfn.org's change on OpenFn.org

--- jobs/f1-j2-casesToOscar.js
+++ jobs/f1-j2-casesToOscar.js
@@ -39,7 +39,7 @@ post(
             //oscar_field, primero_field,
             external_id: c.case_id,
             external_id_display: c.case_id_display,
-            global_id: '',
+            //global_id: '',
             //global_id: '01E6ASHST25VB0BW513KNRJT3E', //this works
             //global_id: c.oscar_number, //remove? 
             mosvy_number: c.mosvy_number,
