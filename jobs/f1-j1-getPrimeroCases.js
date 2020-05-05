5492f04cf2ab4cb3b7c8d1ffba77974b37ae9eee
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 13:42:17 -0600

    don't update lastFetched if no cases found #18

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -7,10 +7,12 @@ getCases(
       transitions_created_at: `date_range||${
         state.lastFetch || '01-01-2020' //default starting date
       }.01-01-4000`, //effectively no end date (year 4000!)
-      //Replace date filter with transitions_changed_at once Quoin confirms questions
-      /*transitions_changed_at: `date_range||${ 
-        state.lastFetch || '01-01-2020' 
-      }.01-01-4000`,*/ // effectively no end date (year 4000!)
+
+      // Replace date filter with transitions_changed_at once Quoin confirms questions
+      // transitions_changed_at: `date_range||${
+      //   state.lastFetch || '01-01-2020'
+      // }.01-01-4000`,
+
       service_response_types: 'list||referral_to_oscar',
     },
   },
@@ -23,10 +25,14 @@ getCases(
         // Get latest transition from a single case
         return x.transitions.map(t => t.created_at).sort((a, b) => b - a)[0];
       })
-      .sort((a, b) => b - a)[0]
-      .split('/');
+      .sort((a, b) => b - a);
 
-    state.lastFetch = `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`;
+    const dateParts = dateArr.length > 0 && dateArr[0].split('/');
+
+    if (dateParts) {
+      console.log("Found cases, updating 'last fetched' date.");
+      state.lastFetch = `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`;
+    }
 
     console.log('The last fetched transition date is: ' + state.lastFetch);
     return state;
