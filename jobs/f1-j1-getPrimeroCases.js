9ab51483a8353568d38f73d80451b9d544183e22
Aleksa Krolls <aleksagk@gmail.com>
2020-05-13 05:44:13 -0700

    aleksa@openfn.org's change on OpenFn.org

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -4,17 +4,17 @@ getCases(
   {
     remote: true,
     scope: {
-      transitions_created_at: `date_range||${
+      /*transitions_created_at: `date_range||${
           state.lastCreated || '01-01-2020'
-        }.01-01-4020`,
-      /*or: {  //TO DISCUSS --> date filters, OR operator
+        }.01-01-4020`,*/
+      or: {  //TO DISCUSS --> date filters, OR operator
         transitions_created_at: `or_op||date_range||${
           state.lastCreated || '01-01-2020'
         }.01-01-4020`,
         transitions_changed_at: `or_op||date_range||${
-          state.lastUpdated || '01-01-2020'
-        }.01-01-4020`,
-      }, */
+          state.lastUpdated || '01-01-2020 00:00'
+        }.01-01-4020 00:00`,
+      }, 
       service_response_types: 'list||referral_to_oscar',
     },
   },
@@ -34,7 +34,7 @@ getCases(
       console.log("Found cases, updating 'last created case' date.");
       state.lastCreated = `${lastCreationParts[2]}-${lastCreationParts[1]}-${lastCreationParts[0]}`;
     }
-    /* Do we need to check for transitions_changed_at date specifically?
+    // Do we need to check for transitions_changed_at date specifically?
     const updateDates = state.data
       .map(x => {
         // Get latest transition from a single case
@@ -49,9 +49,7 @@ getCases(
       state.lastUpdated = `${lastCreationParts[2]}-${lastCreationParts[1]}-${lastCreationParts[0]}`;
     } 
      console.log('The last transition update is: ' + state.lastUpdated);
-    */
-
-    console.log('The last transition creation is: ' + state.lastCreated);
+     console.log('The last transition creation is: ' + state.lastCreated);
 
     return state;
   }
