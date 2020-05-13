33ba34b06f1e3e08caec6b583a4c4c7d90b89b28
Taylor Downs <downs.taylor@gmail.com>
2020-05-13 15:55:02 -0600

    aggregate oscar service_subtype to primero picklist

--- jobs/f2-j2-upsertCasesToPrimero.js
+++ jobs/f2-j2-upsertCasesToPrimero.js
@@ -2,18 +2,203 @@
 // User Story 2: 'View Oscar cases in Primero' AND User Story 4: 'Sending referrals to Primero'
 alterState(state => {
   const serviceMap = {
-    'Social Work / Case Work': 'social_work_case_work',
-    'Family Based Care': 'family_based_care',
-    'Drug/Alcohol': 'drug_alcohol',
-    Counselling: 'counselling',
-    'Financial Development': 'financial_development',
-    'Disability Support': 'disability_support',
-    'Medical Support': 'medical_support',
-    'Legal Support': 'legal_support',
-    'Mental Health Support': 'mental_health_support',
-    'Training and Education': 'training_education',
-    'Family Support': 'family_support',
-    'Anti-Trafficking': 'anti_trafficking',
+    'Generalist social work / case work': {
+      subtype: 'social_work_case_work_generalist',
+      type: 'social_work_case_work',
+    },
+    'Community social work': {
+      subtype: 'social_work_case_work_community',
+      type: 'family_based_care',
+    },
+    'Emergency foster care': {
+      subtype: 'family_based_care_emergency_foster',
+      type: 'family_based_care',
+    },
+    'Long term foster care': {
+      subtype: 'family_based_care_longterm_foster',
+      type: 'family_based_care',
+    },
+    'Kinship care': {
+      subtype: 'family_based_care_kinship',
+      type: 'family_based_care',
+    },
+    'Domestic adoption support': {
+      subtype: 'family_based_care_domestic_adoption',
+      type: 'family_based_care',
+    },
+    'Family preservation': {
+      subtype: 'family_based_care_family_preservation',
+      type: 'family_based_care',
+    },
+    'Family reunification': {
+      subtype: 'family_based_care_family_reunification',
+      type: 'family_based_care',
+    },
+    'Independent Living': {
+      subtype: 'family_based_care_independent_living',
+      type: 'family_based_care',
+    },
+    'Drug and Alcohol Counselling': {
+      subtype: 'drug_alcohol_counselling',
+      type: 'drug_alcohol',
+    },
+    'Detox / rehabilitation services': {
+      subtype: 'drug_alcohol_detox_rehabilitation',
+      type: 'drug_alcohol',
+    },
+    'Detox support': {
+      subtype: 'drug_alcohol_detox_support',
+      type: 'drug_alcohol',
+    },
+    'Generalist counselling': {
+      subtype: 'counselling_generalist',
+      type: 'counselling',
+    },
+    'Counselling for abuse survivors': {
+      subtype: 'counselling_abuse_survivors',
+      type: 'counselling',
+    },
+    'Trauma counselling': {
+      subtype: 'counselling_trauma',
+      type: 'counselling',
+    },
+    'Family counselling / mediation': {
+      subtype: 'counselling_family',
+      type: 'counselling',
+    },
+    'Direct material assistance': {
+      subtype: 'financial_development_material_assistance',
+      type: 'financial_development',
+    },
+    'Direct financial assistance': {
+      subtype: 'financial_development_financial_assistance',
+      type: 'financial_development',
+    },
+    'Income generation services': {
+      subtype: 'financial_development_income_generation',
+      type: 'financial_development',
+    },
+    'Day care services': {
+      subtype: 'financial_development_day_care',
+      type: 'financial_development',
+    },
+    'Therapeutic interventions': {
+      subtype: 'disability_support_therapeutic_interventions',
+      type: 'disability_support',
+    },
+    'Disability respite care': {
+      subtype: 'disability_support_respite-care',
+      type: 'disability_support',
+    },
+    'Therapeutic training': {
+      subtype: 'disability_support_therapeutic_training',
+      type: 'disability_support',
+    },
+    'Disability-aid provision': {
+      subtype: 'disability_support_aid_provision',
+      type: 'disability_support',
+    },
+    'Peripheral supports': {
+      subtype: 'disability_support_peripheral',
+      type: 'disability_support',
+    },
+    'Support groups': {
+      subtype: 'disability_support_groups',
+      type: 'disability_support',
+    },
+    'Support to access care': {
+      subtype: 'medical_support_access_care',
+      type: 'medical_support',
+    },
+    'Provision of medical care': {
+      subtype: 'medical_support_provision_medical_case',
+      type: 'medical_support',
+    },
+    'Medical training services': {
+      subtype: 'medical_support_medical_training',
+      type: 'medical_support',
+    },
+    'Health education': {
+      subtype: 'medical_support_healt_education',
+      type: 'medical_support',
+    },
+    'Support to access legal services': {
+      subtype: 'legal_support_access_legal_services',
+      type: 'legal_support',
+    },
+    'Legal advocacy services': {
+      subtype: 'legal_support_advocacy_services',
+      type: 'legal_support',
+    },
+    'Legal representation': {
+      subtype: 'legal_support_representation',
+      type: 'legal_support',
+    },
+    'Prison visitation support': {
+      subtype: 'legal_support_prision_visitation',
+      type: 'legal_support',
+    },
+    'Therapeutic interventions': {
+      subtype: 'mental_health_support_therapeutic_interventions',
+      type: 'mental_health_support',
+    },
+    'Therapeutic training': {
+      subtype: 'mental_health_support_therapeutic_training',
+      type: 'mental_health_support',
+    },
+    'Direct medical support': {
+      subtype: 'mental_health_support_direct_medical_support',
+      type: 'mental_health_support',
+    },
+    'School support': {
+      subtype: 'training_education_school_support',
+      type: 'training_education',
+    },
+    'Supplementary school education': {
+      subtype: 'training_education_supplementary',
+      type: 'training_education',
+    },
+    'Vocational education and training': {
+      subtype: 'training_education_vocational',
+      type: 'training_education',
+    },
+    'Material support for education (uniforms, etc)': {
+      subtype: 'training_education_material_support',
+      type: 'training_education',
+    },
+    'Scholarships or financial support': {
+      subtype: 'training_education_scholarships',
+      type: 'training_education',
+    },
+    'Life skills': {
+      subtype: 'training_education_life_skills',
+      type: 'training_education',
+    },
+    'Family support': {
+      subtype: 'family_support_family_support',
+      type: 'family_support',
+    },
+    'Rescue Services': {
+      subtype: 'anti_trafficking_rescue',
+      type: 'anti_trafficking',
+    },
+    'Transitional Accommodation': {
+      subtype: 'anti_trafficking_transitional_accomodation',
+      type: 'anti_trafficking',
+    },
+    'Post-Trafficking Counseling': {
+      subtype: 'anti_trafficking_post_trafficking',
+      type: 'anti_trafficking',
+    },
+    'Community Reintegration Support': {
+      subtype: 'anti_trafficking_community_reintegration',
+      type: 'anti_trafficking',
+    },
+    'Residential Care Institution': {
+      subtype: 'residential_care_gov_only_other',
+      type: 'other',
+    },
+    'Other Service': { subtype: 'other_other_service', type: 'other' },
   };
 
   const agencyMap = {
@@ -84,6 +269,39 @@ alterState(state => {
   };
 
   state.cases = state.data.data.map(c => {
+    function convert(arr) {
+      const obj = arr
+        .map(s => {
+          return {
+            unique_id: s.uuid,
+            service_subtype:
+              (serviceMap[s.name] && serviceMap[s.name].subtype) || 'Other',
+            service_type:
+              (serviceMap[s.name] && serviceMap[s.name].type) || 'Other',
+            service_type_text:
+              (serviceMap[s.name] && serviceMap[s.name].type) || 'Other',
+            service_type_details_text: serviceMap[s.name] ? 'n/a' : s.name,
+          };
+        })
+        .reduce((result, currentValue) => {
+          (result[currentValue['service_type']] =
+            result[currentValue['service_type']] || []).push(currentValue);
+          return result;
+        }, {});
+
+      const newArr = Object.keys(obj).map(key => {
+        return {
+          unique_id: obj[key][0].unique_id,
+          service_subtype: obj[key].map(st => st.service_subtype),
+          service_type: key,
+          service_type_text: key,
+          service_type_details_text: obj[key][0].service_type_details_text,
+        };
+      });
+
+      return newArr;
+    }
+
     //Mappings for upserting cases in Primero (update if existing, insert if new)
     return {
       remote: true,
@@ -113,18 +331,11 @@ alterState(state => {
         oscar_referring_organization: c.organization_name,
         oscar_case_worker_telephone: c.case_worker_mobile,
       },
-      services_section: c.services.map(s => {
+      services_section: convert(c.services),
+      transitions: convert(c.services).map(t => {
         return {
-          unique_id: s.uuid,
-          service_type: serviceMap[s.name] || 'Other', //Add service mapping table. If s.name not in table, list as "Other"
-          service_type_text: serviceMap[s.name] || 'Other', //Same logic as above
-          service_type_details_text: serviceMap[s.name] ? 'n/a' : s.name, //If type is "Other", map to services[][name][][subservices][name]
-        };
-      }),
-      transitions: c.services.map(t => {
-        return {
-          service_section_unique_id: t.uuid,
-          service: serviceMap[t.name] || 'Other', //This is see above logic for service name mapping, array
+          service_section_unique_id: t.unique_id,
+          service: t.name || 'Other',
           // created_at: t.referral_date, //Q: Should this be today's date?
           type: 'referral',
         };
