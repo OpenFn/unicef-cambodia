909ff95b4e35d880d01039b5ed299288f79d0f9f
Taylor Downs <downs.taylor@gmail.com>
2020-06-19 08:44:25 -0600

    more on #27

--- jobs/f2-j2-upsertCasesToPrimero.js
+++ jobs/f2-j2-upsertCasesToPrimero.js
@@ -312,7 +312,7 @@ alterState(state => {
       oscar_number: c.global_id,
       child: {
         // primero_field: oscar_field,
-        case_id: c.global_id, // externalId for upsert (will fail if multiple found)
+        case_id: c.external_id, // externalId for upsert (will fail if multiple found)
         oscar_number: c.global_id,
         mosvy_number: c.mosvy_number,
         name_first: c.given_name,
