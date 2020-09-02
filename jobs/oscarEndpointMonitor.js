f7efda95d18a526e4501fbc686590cd823eba183
Taylor Downs <taylor@openfn.org>
2020-09-02 07:24:43 -0400

    Create oscarEndpointMonitor.js

---
+++ jobs/oscarEndpointMonitor.js
@@ -0,0 +1,16 @@
+post(
+  //Oscar authentication  --> To update?
+  '/api/v1/auth/sign_in',
+  {
+    keepCookie: true,
+    body: {
+      email: state.configuration.username,
+      password: state.configuration.password,
+    },
+  },
+  state => {
+  state.body = {}
+  state.references = []
+  return state;
+  }
+);
