6083633e5d7178dd2a752be73d7062a65070f784
Taylor Downs <downs.taylor@gmail.com>
2020-09-29 16:02:40 +0100

    modify cursor for new primero response, closes #56

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -15,10 +15,10 @@ getCases(
       or: {
         //Two case date fields we must check for updates
         transitions_created_at: `or_op||date_range||${
-          '25-09-2020'//state.lastCreated || '10-09-2020' //TEST CURSOS
+          state.lastCreated || '25-09-2020' // TEST CURSOR
         }.01-01-4020`,
         transitions_changed_at: `or_op||date_range||${
-          '25-09-2020 00:00'//state.lastUpdated || '10-09-2020 00:00'//TEST CURSOS
+          state.lastUpdated || '25-09-2020 00:00' // TEST CURSOR
         }.01-01-4020 00:00`,
       },
       oscar_number: 'range||*.*', //new filter to fetch ALL oscar cases
@@ -31,8 +31,14 @@ getCases(
       `Primero API responded with cases: ${JSON.stringify(state.data.map(x => x.case_id_display))}`
     );
 
+    function parseDates(str) {
+      const [y, m, dtz] = str.split('/');
+      const [d, t, z] = dtz.split(' ');
+      return { y, m, d, t, z };
+    }
+
     // Get latest transition from all cases.
-    const creationDates = state.data
+    const lastCreation = state.data
       .map(c => {
         if (c.transitions && c.transitions.length > 0) {
           // Get latest transition from a single case
@@ -40,27 +46,24 @@ getCases(
         }
         return c.transitions_changed_at;
       })
-      .sort((a, b) => b - a);
+      .sort((a, b) => b - a)[0];
 
-    const lastCreationParts = creationDates[0] && creationDates[0].split('/');
-
-    if (lastCreationParts) {
+    if (lastCreation) {
       console.log("Found new cases, updating 'last created case' date.");
-      state.lastCreated = `${lastCreationParts[2]}-${lastCreationParts[1]}-${lastCreationParts[0]}`;
+      var { y, m, d } = parseDates(lastCreation);
+      state.lastCreated = `${d}-${m}-${y}`;
     }
 
-    const updateDates = state.data
+    const lastUpdate = state.data
       .filter(x => x.transitions_changed_at)
       .map(x => x.transitions_changed_at)
-      .sort((a, b) => b - a);
+      .sort((a, b) => b - a)[0];
 
-    const lastUpdateParts = updateDates[0] && updateDates[0].split('/');
-
-    if (lastUpdateParts) {
+    if (lastUpdate) {
       console.log(`Found cases w/ updates, updating 'last updated case' date.`);
-      const lastUpdateDay = lastUpdateParts[2].split(' ')[0];
-      const lastUpdateTimes = lastUpdateParts[2].split(' ')[1].split(':');
-      state.lastUpdated = `${lastUpdateDay}-${lastUpdateParts[1]}-${lastUpdateParts[0]} ${lastUpdateTimes[0]}:${lastUpdateTimes[1]}`;
+      var { y, m, d, t } = parseDates(lastUpdate);
+      var [h, m, s] = t.split(':');
+      state.lastUpdated = `${d}-${m}-${y} ${h}:${m}`;
     }
 
     console.log('The last transition update is now: ' + state.lastUpdated);
