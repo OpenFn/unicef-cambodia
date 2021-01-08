a0b873e2337b8d891e763a5c25d5850bc2cb5638
Taylor Downs <downs.taylor@gmail.com>
2021-01-08 08:55:39 +0000

    shift org setting into function on state for #70

--- jobs/f1-j2-casesToOscar.js
+++ jobs/f1-j2-casesToOscar.js
@@ -31,6 +31,19 @@ alterState(state => {
     }
   };
 
+  state.setOrganization = c => {
+    if (c.services_section.some(s => s.service_response_type === 'referral_from_oscar')) {
+      // NOTE: The "business" stipulates that all "oscar_referring_organization"
+      // values will be the SAME for an array including any "referral_from_oscar"
+      // items, and requires that we merely select the first item.
+      return c.services_section[0].oscar_referring_organization.substring(7);
+    } else if (c.services_section.some(s => s.service_response_type === 'referral_to_oscar')) {
+      return c.services_section[0].service_implementing_agency.substring(7);
+    } else {
+      return c.owned_by_agency.substring(7);
+    }
+  };
+
   return state;
 });
 
@@ -66,7 +79,7 @@ post(
         };
       },
       body: state => {
-        const { oscarStrings } = state;
+        const { oscarStrings, setOrganization } = state;
 
         function checkValue(data) {
           if (data !== 'NaN' && data) {
@@ -189,20 +202,7 @@ post(
           external_case_worker_name: oscarStrings(c.owned_by),
           external_case_worker_id: oscarStrings(c.owned_by_id),
           external_case_worker_mobile: c.owned_by_phone || '000000000',
-          organization_name: state => {
-            if (c.services_section.some(s => s.service_response_type === 'referral_from_oscar')) {
-              // NOTE: The "business" stipulates that all "oscar_referring_organization"
-              // values will be the SAME for an array including any "referral_from_oscar"
-              // items, and requires that we merely select the first item.
-              return c.services_section[0].oscar_referring_organization.substring(7);
-            } else if (
-              c.services_section.some(s => s.service_response_type === 'referral_to_oscar')
-            ) {
-              return c.services_section[0].service_implementing_agency.substring(7);
-            } else {
-              return c.owned_by_agency.substring(7);
-            }
-          },
+          organization_name: setOrganization(c),
           organization_id: oscarStrings(c.owned_by_agency_id), //Q: replace with service_implementing_agency ??
           is_referred: true,
           services: c.services_section
@@ -219,10 +219,12 @@ post(
           transaction_id: c.transition_id,
           // transaction_id: oscarStrings(c.transition_id),
         };
+
         console.warn(
           'FULL LOGS FOR TESTING ONLY!!!: ',
           JSON.stringify({ organization: oscar }, null, 2)
         );
+
         // NOTE: Logs for enhanced audit trail.
         console.log(
           'Case data to be posted to Oscar: ',
