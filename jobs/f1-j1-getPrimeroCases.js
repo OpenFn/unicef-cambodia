790f407687bfba4882451c9cf144dfb1e1430e8e
Aleksa Krolls <aleksagk@gmail.com>
2021-01-18 12:36:03 +0200

    Update f1-j1-getPrimeroCases.js

--- jobs/f1-j1-getPrimeroCases.js
+++ jobs/f1-j1-getPrimeroCases.js
@@ -24,7 +24,8 @@ getCases(
         }.01-01-4020 00:00`,
       },
       service_response_types: 'list||referral_to_oscar', // only cases with referral services
-      record_state: true //only fetch active cases
+      record_state: true, //only fetch active cases
+      child_status: 'open' //only fetch open cases
     },
     per: 1000,
   },
