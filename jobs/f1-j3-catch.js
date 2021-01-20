5953610a95fd8041553dbe0fde353e522efee125
Taylor Downs <downs.taylor@gmail.com>
2021-01-20 09:07:44 +0000

    typo in f1-j3 logs

--- jobs/f1-j3-catch.js
+++ jobs/f1-j3-catch.js
@@ -1,5 +1,5 @@
 console.log('Sending cases to Oscar failed.')
-console.log('Posting initial cursor back to inbox to initate another Primero fetch.')
+console.log('Posting initial cursor back to inbox to initiate another Primero fetch.')
 post(
   state.configuration.inboxId, // inbox uuid
   {
