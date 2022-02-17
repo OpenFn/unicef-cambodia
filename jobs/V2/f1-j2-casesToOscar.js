78429633c1fde94f7abf3769ac6fad75fb97a320
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-02-17 16:49:47 +0000

    aissatou@openfn.org's change on OpenFn.org

--- jobs/V2/f1-j2-casesToOscar.js
+++ jobs/V2/f1-j2-casesToOscar.js
@@ -336,6 +336,7 @@ alterState(state => {
               is_referred: false,
             })),
           };
+          console.log(state.data);
           console.log("'Update links' with non-referrals:", payload);
           return payload;
         },
