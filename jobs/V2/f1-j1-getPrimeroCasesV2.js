d53eeed36fe4ac295afa0cb2377067b677c8fd88
Taylor Downs <taylor@openfn.org>
2022-04-02 10:18:33 +0100

    clear unused cursor for #102

--- jobs/V2/f1-j1-getPrimeroCasesV2.js
+++ jobs/V2/f1-j1-getPrimeroCasesV2.js
@@ -1,13 +1,4 @@
-// If the job was executed from a message in the inbox with a specific cursor,
-// use that. If not, use the cursor from the previous final state.
-fn(state => {
-  if (state.data && state.data.initialState) {
-    const { lastCaseCreated, lastUpdated, lastCreated } = state.data.initialState;
-    return { ...state, lastCaseCreated, lastUpdated, lastCreated };
-  }
-  return state;
-});
-
+// Either use a manual cursor, or take the cursor from the last run.
 fn(state => {
   console.log('Last sync end date:', state.lastRunDateTime);
   const manualCursor = '2021-12-08T00:00:00.000Z';
@@ -16,18 +7,7 @@ fn(state => {
 });
 
 // Clear data from previous runs.
-fn(state => {
-  // TODO: Can we get rid of all references to lastCaseCreated, lastUpdated, and lastCreated?
-  // This seems like legacy code. We no longer use these to create cursor (see L14).
-  const { lastCaseCreated, lastUpdated, lastCreated } = state;
-  console.log('The last case creation before this run is:', lastCaseCreated);
-  console.log('The last case update before this run is:', lastUpdated);
-  console.log('The last transition creation before this run is:', lastCreated);
-
-  const initialState = { lastCaseCreated, lastUpdated, lastCreated };
-
-  return { ...state, data: {}, references: [], initialState };
-});
+fn(state => ({ ...state, data: {}, references: [] }));
 
 // GET Primero cases with oscar referrals
 // User Story 1: Generating government referrals
@@ -59,61 +39,10 @@ fn(state => {
     state => {
       console.log(`Other cases: ${JSON.stringify(state.data.map(x => x.case_id_display))}`);
 
-      // #3 - Combine cases, then identify last timestamps and reformat them =====
+      // #3 - Combine cases =====
       state.data = state.data.concat(state.oscarRefs);
       delete state.oscarRefs;
 
-      function parseDates(str) {
-        const date = str.split('T')[0];
-        const time = str.split('T')[1];
-        const [y, m, d] = date.split('-');
-        const [hr, min, sec] = time.split(':');
-        return { y, m, d, hr, min, sec };
-      }
-
-      // Get latest case creation from all cases. ================================
-      const lastCaseCreation = state.data.map(c => c.created_at).sort((a, b) => b - a)[0];
-
-      if (lastCaseCreation) {
-        console.log(
-          `Found new cases w/o transitions, updating 'last case creation' date using ${lastCaseCreation}.`
-        );
-        var { y, m, d } = parseDates(lastCaseCreation);
-        state.lastCaseCreated = `${d}-${m}-${y}`;
-      }
-      // =========================================================================
-
-      // Get latest transition from all cases. ===================================
-      const lastCreation = state.data
-        .map(c => {
-          if (c.transitions && c.transitions.length > 0) {
-            // Get latest transition from a single case
-            return c.transitions.map(t => t.created_at).sort((a, b) => b - a)[0];
-          }
-          return c.transitions_changed_at;
-        })
-        .sort((a, b) => b - a)[0];
-
-      if (lastCreation) {
-        console.log(`Found new cases, updating 'last created case' date using ${lastCreation}.`);
-        var { y, m, d } = parseDates(lastCreation);
-        state.lastCreated = `${d}-${m}-${y}`;
-      }
-
-      const lastUpdate = state.data.map(c => c.last_updated_at).sort((a, b) => b - a)[0];
-
-      if (lastUpdate) {
-        console.log(
-          `Found cases w/ updates, updating 'last updated case' date using ${lastUpdate}.`
-        );
-        var { y, m, d, hr, min } = parseDates(lastUpdate);
-        state.lastUpdated = `${d}-${m}-${y} ${hr}:${min}`;
-      }
-      // =========================================================================
-
-      console.log('The last case creation is now: ' + state.lastCaseCreated);
-      console.log('The last case update is now: ' + state.lastUpdated);
-      console.log('The last transition creation is now: ' + state.lastCreated);
       return state;
     }
   )(state);
