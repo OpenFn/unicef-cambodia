ae807616b1b7c64c4e5b11eb5a03607c6a0a4252
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-04-11 07:39:09 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/V2/f2-j2-upsertCasesToPrimero.js
+++ jobs/V2/f2-j2-upsertCasesToPrimero.js
@@ -545,6 +545,7 @@ each(
   upsertCase({
     externalIds: ['case_id'],
     data: state => {
+      console.log(JSON.stringify(state.data, null, 2));
       delete state.data.__original_oscar_record;
       return state.data;
     },
