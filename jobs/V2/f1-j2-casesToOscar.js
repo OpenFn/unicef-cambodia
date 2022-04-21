7b0c3d34ef02ccf849ab93a2888afb0a15633266
Aleksa Krolls <aleksagk@gmail.com>
2022-04-21 09:38:09 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/V2/f1-j2-casesToOscar.js
+++ jobs/V2/f1-j2-casesToOscar.js
@@ -18,8 +18,7 @@ fn(state => {
   const isRefferal = ({ services_section }) => {
     console.log(`services_section to evaluate: ${JSON.stringify(services_section, null, 4)}`);
     const responseTypes = services_section.map(s => s.service_response_type);
-     console.log("responsetypes", responseTypes);
-     console.log("services section", services_section);
+    console.log("responsetypes", responseTypes);
     return (
       responseTypes.includes('referral_to_oscar') || responseTypes.includes('referral_from_oscar')
     );
