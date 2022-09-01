8bb523b5886c8c99675f7b4f70f66314f8eb2b40
Aleksa Krolls <aleksagk@gmail.com>
2022-09-01 09:07:54 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/V2/f2-j1-getOscarCases.js
+++ jobs/V2/f2-j1-getOscarCases.js
@@ -53,7 +53,7 @@ post(
       }),
       query: {
         // NOTE: since_date must be rounded to 00:00:00 to work with Oscar API.
-        since_date: state.lastQueryDate || '2022-08-26 00:00:00',
+        since_date: state.lastQueryDate || '2022-08-31 00:00:00',
         //referred_external: true, //old query parameter - to remove to pull ALL cases, not just referrals
       },
     },
