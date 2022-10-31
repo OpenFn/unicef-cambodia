4cf20c853d4b076287bf7f052a16cee555c72684
Aleksa Krolls <aleksagk@gmail.com>
2022-10-31 19:27:18 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/V2/f1-j1-getPrimeroCasesV2.js
+++ jobs/V2/f1-j1-getPrimeroCasesV2.js
@@ -5,7 +5,7 @@ fn(state => {
   // console.log('Flow 0 oscarCursor:: ', oscarCursor);
 
   console.log('Last sync end date:', state.lastRunDateTime || 'undefined; using manual cursor...');
-  const manualCursor = '2022-10-25T23:56:07.539Z'; //'2022-09-07T08:57:24.777Z';
+  const manualCursor = '2022-10-31T20:56:07.539Z'; //'2022-09-07T08:57:24.777Z';
   const currentAttempt = new Date().toISOString();
   console.log('Current attempt time:', currentAttempt);
 
