9685bf89381a2840ad5f97a131a890d322977990
Aleksa Krolls <aleksagk@gmail.com>
2023-07-04 11:55:23 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/V2/f1-j1-getPrimeroCasesV2.js
+++ jobs/V2/f1-j1-getPrimeroCasesV2.js
@@ -1,7 +1,7 @@
 // Either use a manual cursor, or take the cursor from the last run.
 fn(state => {
   console.log('Last sync end date:', state.lastRunDateTime || 'undefined; using manual cursor...');
-  const manualCursor = '2023-06-28T00:40:00.862Z';
+  const manualCursor = '2023-06-29T00:40:00.862Z';
   const currentAttempt = new Date().toISOString();
   console.log('Current attempt time:', currentAttempt);
 
